5559c7be1d873d461f3b7e34ed203744c6aafff9
Filippo <f.storniolo@hotmail.it>
2021-12-28 12:44:10 -0800

    device struct is now in .h file

--- hw/misc/newdev.c
+++ hw/misc/newdev.c
@@ -40,7 +40,7 @@
 #include <sys/socket.h> 
 
 #include "hw/misc/bpf_injection_msg.h"
-
+#include "hw/misc/newdev.h"
 #include "hw/core/cpu.h"
 
 //Affinity part
@@ -62,93 +62,12 @@
 #define DBG(fmt, ...) do {} while (0)
 #endif
 
-#define TYPE_NEWDEV_DEVICE "newdev"
-#define NEWDEV(obj)        OBJECT_CHECK(NewdevState, obj, TYPE_NEWDEV_DEVICE)
-
-#define NEWDEV_REG_PCI_BAR      0
-#define NEWDEV_BUF_PCI_BAR      1
-#define NEWDEV_REG_END          92
-#define NEWDEV_REG_MASK         0xff
-#define NEWDEV_BUF_MASK         0xffff
-#define NEWDEV_BUF_SIZE         65536
-
-
-//not used, misleading. OLD used only in ioregs R/W
-#define NEWDEV_REG_STATUS_IRQ   0
-#define NEWDEV_REG_RAISE_IRQ    8
-#define NEWDEV_REG_LOWER_IRQ    12
-
-// DEVICE BUFMMIO STRUCTURE. OFFSET IN #bytes/sizeof(uint32_t)
-
-// +---+--------------------------------+
-// | 0 | irq_status [R] / raise_irq [W] |
-// +---+--------------------------------+
-// | 1 |          lower_irq [W]         |
-// +---+--------------------------------+
-// | 2 |      unspecified/reserved      |
-// +---+--------------------------------+
-// | 3 |      unspecified/reserved      |
-// +---+--------------------------------+
-// | 4 |                                |
-// +---+                                |
-// | 5 |            buffer              |
-// +---+                                |
-// |   |                                |
-// |   |                                |
-//                  ......
-// |   |                                |
-// |   |                                |
-// +---+--------------------------------+
-
-
-
-
 static const char *regnames[] = {
     "STATUS",
     "CTRL",
     "RAISE_IRQ",
     "LOWER_IRQ",
 };
-
-typedef struct NewdevTranslateEntry {
-    uint64_t gpa_start;
-    uint64_t gpa_end;
-    uint64_t size;
-    void *hva_start;
-    MemoryRegion *mr;
-} NewdevTranslateEntry;
-
-typedef struct {
-    PCIDevice pdev;
-    MemoryRegion regs;
-    MemoryRegion mmio;
-
-    // Used for address translation
-    MemoryListener memory_listener;
-    NewdevTranslateEntry *trans_entries;
-    unsigned int num_trans_entries;
-    NewdevTranslateEntry *trans_entries_tmp;
-    unsigned int num_trans_entries_tmp;
-
-    /* Storage for the I/O registers. */
-    uint32_t ioregs[NEWDEV_REG_END >> 2];
-
-    /* Storage for the buffer. */
-    uint32_t *buf;
-
-    QemuThread thread;
-    QemuMutex thr_mutex;
-    QemuCond thr_cond;
-    bool stopping;
-    uint32_t irq_status;
-
-
-    bool hyperthreading_remapping; 
-    
-    int listen_fd;  //listening socket fd
-    int connect_fd; //connected socket fd (use for command exchange)
-
-} NewdevState;
 
 static void newdev_raise_irq(NewdevState *newdev, uint32_t val);
 static void connected_handle_read(void *opaque);
